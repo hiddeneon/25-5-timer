21a9914a94bc9c4d651b2917f4ccc3df97e230ea
keinePP <alkalea@protonmail.com>
2023-08-02 14:03:53 +0300

    feat: break/session label changes color at 1:00

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,21 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { css } from "@emotion/react";
 
 export default function Timer({ minutes, seconds, isPeriod }) {
+
+    const alertStyle = css({
+        color: 'red'
+    });
+
+    const normalStyle = css({
+        color: 'white'
+    })
+
     return (
         <div class="timer-body">
             <label id="timer-label">{isPeriod}</label>
-            <div id="time-left">{minutes}:{seconds}</div>
+            <div css={minutes === '00' || (minutes === '01' && seconds === '00') ? alertStyle : normalStyle} id="time-left">{minutes}:{seconds}</div>
         </div>
     )
 }
